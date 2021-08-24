53e67e67d7db05a7281bd0a712a916f8bc6158ba
Dodsy <halseyng@outlook.com>
2021-08-24 12:36:38 -0400

    Added a sudoku puzzle generator

--- src/GeneratePuzzle.java
+++ src/GeneratePuzzle.java
@@ -1,14 +1,14 @@
 import java.io.*;
 import java.net.*;
-import org.json.simple.parser.*;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONValue;
+import org.json.*;
 public class GeneratePuzzle {
     private String response;
-    public GeneratePuzzle(int size, int level){
+    private int[][] puzzle;
+    public GeneratePuzzle(int level){
         String FORMAT = "http://www.cs.utep.edu/cheon/ws/sudoku/new/?size=%d&level=%d";
-        String url = String.format(FORMAT,size,level);
+        String url = String.format(FORMAT,9,level);
         response = sendGet(url);
+        puzzle = new int[9][9];
     }
     private String sendGet(String urlString){
         HttpURLConnection con = null;
@@ -31,27 +31,36 @@ public class GeneratePuzzle {
         }
         return null;
     }
-    public String getResponse(){
-        return response;
-    }
     public void newPuzzle(int size, int level){
         String FORMAT = "http://www.cs.utep.edu/cheon/ws/sudoku/new/?size=%d&level=%d";
         String url = String.format(FORMAT,size,level);
         response = sendGet(url);
+
+        JSONObject obj = new JSONObject(response);
+        JSONArray arr = obj.getJSONArray("squares");
+
+        for (int i = 0; i < arr.length(); i++){
+            JSONObject curr = arr.getJSONObject(i);
+            puzzle[curr.getInt("x")][curr.getInt("y")] = curr.getInt("value");
+        }
+     
+
+
+
+    }
+    public int[][] getPuzzle(){
+        return puzzle;
     }
     public static void main(String[] args) {
-        GeneratePuzzle puzzle = new GeneratePuzzle(4, 1);
-        System.out.println(puzzle.response);
-        JSONParser parser = new JSONParser();
-        Object obj;
-        try {
-            obj = parser.parse(puzzle.response);
-            JSONArray array = new JSONArray();
-            array.add(obj);
-        } catch (ParseException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        GeneratePuzzle puzzle = new GeneratePuzzle(1);
+        puzzle.newPuzzle(9, 1);
+        for (int i = 0; i < puzzle.puzzle.length; i++){
+            for (int j = 0; j < puzzle.puzzle[i].length; j++){
+                System.out.print(puzzle.puzzle[i][j]+" ");
+            }
+            System.out.println();
         }
+        
         
 
         
