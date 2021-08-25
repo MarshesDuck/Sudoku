7c07198df152911edf31a3cff4fc5fc84b567087
Dodsy <halseyng@outlook.com>
2021-08-25 14:48:47 -0400

    Moved logic from here to Game.java, changed this from frame to panel

--- src/mainMenu.java
+++ src/mainMenu.java
@@ -1,42 +1,32 @@
 import javax.swing.*;
-import java.io.IOException;
-
-public class mainMenu extends JFrame {
+public class mainMenu extends JPanel {
     JButton startButton;
     JButton optionButton;
     public mainMenu(){
         ImageImplement panel = new ImageImplement(new ImageIcon("assets/titleScreen.png").getImage());
         
-        
-        startButton = new JButton(new ImageIcon("assets/startButton.png"));
-        optionButton = new JButton(new ImageIcon("assets/optionButton.png"));
-
+        startButton = new JButton();
+        optionButton = new JButton();
 
         startButton.setBounds(443,355,130,50);
         optionButton.setBounds(437,420,150,50);
+        
+        startButton.setOpaque(false);
+        startButton.setContentAreaFilled(false);
+        startButton.setBorderPainted(false);
+
+        optionButton.setOpaque(false);
+        optionButton.setContentAreaFilled(false);
+        optionButton.setBorderPainted(false);
   
         add(startButton);
         add(optionButton);
 
         add(panel);
+
         setSize(640,665);
         setLayout(null);
         setVisible(true);
-        setResizable(false);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        BGMPlayer player = new BGMPlayer();
-        player.play("Music/BGM.wav");
-        boolean running = true;
-        while (running){
-            if (player.playCompleted){
-                player.playCompleted = false;
-                player.play("Music/BGM.wav");
-            }
-        }
-
-    }
-    public static void main (String[] args) throws IOException {
-        new mainMenu();
     }
 }
