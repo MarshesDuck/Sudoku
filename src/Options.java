691f773d61a66aa6db697d00a09e7d6e108458c1
Dodsy <halseyng@outlook.com>
2021-08-27 21:51:23 -0400

    probably going to change this later

--- src/Options.java
+++ src/Options.java
@@ -1,9 +1,11 @@
 public class Options {
     private int difficulty;
     private boolean highlighting;
-    public Options(int difficulty,boolean highlighting){
+    private boolean musicOn;
+    public Options(int difficulty,boolean highlighting, boolean musicOn){
         this.difficulty = difficulty;
         this.highlighting = highlighting;
+        this.musicOn = musicOn;
     }
     public void setDifficulty(int difficulty){
         if (difficulty == 0){
@@ -14,11 +16,17 @@ public class Options {
     public void setHighlighting(boolean highlighting){
         this.highlighting = highlighting;
     }
+    public void setMusic(boolean musicOn){
+        this.musicOn = musicOn;
+    }
     public int getDifficulty(){
         return difficulty;
     }
     public boolean getHighlighting(){
         return highlighting;
+    }
+    public boolean getMusic(){
+        return musicOn;
     }
     
 }
